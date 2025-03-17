b9b778adb3f9cf556d7e73fb680d9d171ea5a3ea
bUzR4Ks <aulia.bilibili@gmail.com>
2025-03-17 18:52:53 +0700

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './style/style.css';
-import './components/app-footer.js';
-import './components/note-form.js';
-import './components/save-button.js';
-import './components/loading.js';
-import api from './scripts/main.js';
-
